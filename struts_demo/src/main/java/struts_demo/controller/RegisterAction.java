90580017daa7e27b8f38e891f5d1bb0e7588be5f
Bui Van Lam (VTI.D5.Intern) <lam.buivan@vti.vn>
2019-11-11 15:44:55 +0700

    update validate struts

--- struts_demo/src/main/java/struts_demo/controller/RegisterAction.java
+++ struts_demo/src/main/java/struts_demo/controller/RegisterAction.java
@@ -13,7 +13,25 @@ public class RegisterAction extends ActionSupport {
         person = new Person();
         return SUCCESS;
     }
+    @Override
+    public void validate() {
+        if (person.getFirstName().length() == 0) {
+            addFieldError("person.firstName", "First name is required.");
+        }
 
+        if (person.getEmail().length() == 0) {
+            addFieldError("person.email", "Email is required.");
+        }
+
+        if (person.getAge() < 18) {
+            addFieldError("person.age", "Age is required and must be 18 or older");
+        }
+    }
+
+    @Override
+    public void addFieldError(String fieldName, String errorMessage) {
+        super.addFieldError(fieldName, errorMessage);
+    }
     public Person getPerson() {
         return person;
     }
